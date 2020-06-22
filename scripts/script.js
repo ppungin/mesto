f545fce3bae5615ca98449af69de734d6fc05f6c
Pavel Pungin <ppungin@yandex.ru>
2020-06-22 15:13:36 +0500

    Готово!

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-let content = document.querySelector('.content');
-let editButton = content.querySelector('.profile__edit-button');
-let popUp = content.querySelector('.popup');
-let saveButton = content.querySelector('.popup__button-save');
-let popUpClose = content.querySelector('.popup__close');
-let profileName = content.querySelector('.profile__name');
-let profileProfession = content.querySelector('.profile__profession');
-let inputName = content.querySelector('.input__text_type_name');
-let inputProfession = content.querySelector('.input__text_type_profession');
+const content = document.querySelector('.content');
+const editButton = content.querySelector('.profile__edit-button');
+const popUp = content.querySelector('.popup');
+const saveButton = content.querySelector('.popup__button-save');
+const popUpClose = content.querySelector('.popup__close');
+const profileName = content.querySelector('.profile__name');
+const profileProfession = content.querySelector('.profile__profession');
+const inputName = content.querySelector('.input__text_type_name');
+const inputProfession = content.querySelector('.input__text_type_profession');
 
 function editProfile() {
     popUp.classList.add('popup_opened');
