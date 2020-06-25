1b478bd6b6c952eb310bbc43099f9b8a33011f29
Pavel Pungin <ppungin@yandex.ru>
2020-06-25 22:38:19 +0500

    Правки по списку окончены

--- scripts/script.js
+++ scripts/script.js
@@ -2,32 +2,31 @@ const content = document.querySelector('.root');
 const editButton = content.querySelector('.profile__button_edit');
 const popUp = content.querySelector('.popup');
 const saveButton = content.querySelector('.popup__button-save');
-const popUpClose = content.querySelector('.popup__close');
+const closeIcon = content.querySelector('.popup__close');
 const profileName = content.querySelector('.profile__name');
 const profileProfession = content.querySelector('.profile__profession');
 const inputName = content.querySelector('.popup__input_name');
 const inputProfession = content.querySelector('.popup__input_profession');
+let form = content.querySelector('.popup__container');
 
-function editProfile() {
+
+function popUpOpen() {
     popUp.classList.add('popup_opened');
     inputName.value = profileName.textContent;
     inputProfession.value = profileProfession.textContent;
 }
 
+function popUpClose(){
+    popUp.classList.remove('popup_opened');
+}
 
-function saveProfile(){
+function formSubmitHandler(event){
+    event.preventDefault();
     profileName.textContent = inputName.value;
     profileProfession.textContent = inputProfession.value;
-    inputName.setAttribute('placeholder', inputName.value);
-    inputProfession.setAttribute('placeholder', inputProfession.value);
-    popUp.classList.remove('popup_opened');
+    popUpClose();
 }
 
-function closePopUp(){
-    popUp.classList.remove('popup_opened');
-}
-
-
-editButton.addEventListener('click', editProfile);
-saveButton.addEventListener('click', saveProfile);
-popUpClose.addEventListener('click', closePopUp);
+editButton.addEventListener('click', popUpOpen);
+form.addEventListener('submit', formSubmitHandler);
+closeIcon.addEventListener('click', popUpClose);
