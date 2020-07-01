08b9b3ece51f418290b675039e7a847adf10b858
Pavel Pungin <ppungin@yandex.ru>
2020-07-01 15:45:49 +0500

    Fixed some bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 const content = document.querySelector('.root');
-const editButton = content.querySelector('.profile__button_edit');
+const editButton = content.querySelector('.profile__button-edit');
 const popUp = content.querySelector('.popup');
 const saveButton = content.querySelector('.popup__button-save');
 const closeIcon = content.querySelector('.popup__close');
 const profileName = content.querySelector('.profile__name');
 const profileProfession = content.querySelector('.profile__profession');
 const inputName = content.querySelector('.popup__input_name');
-const inputProfession = content.querySelector('.popup__input_profession');
-const form = content.querySelector('.popup__container');
+const inputProfession = content.querySelector('.popup__input-profession');
+const form = content.querySelector('.popup__form');
 
 
 function togglePopUp() {
